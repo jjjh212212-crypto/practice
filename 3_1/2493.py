197b3f6ca2ec3373d7841e4956c23282a8e25e4c
kjh <jjjh212212@gmail.com>
2026-03-06 17:52:26 +0900

    0306_1

--- 3_1/2493.py
+++ 3_1/2493.py
@@ -1,2 +1,19 @@
 N=int(input())
-lst=[int(input()) for _ in range(N)]
+lst=list(map(int,input().split()))
+stack=[lst[0]]
+idx=[1]
+print(0,end=' ')
+for i in range(1,N): 
+    if stack[-1] <= lst[i]:
+        while stack and stack[-1] <= lst[i]:
+            stack.pop()
+            idx.pop()
+    if not stack:
+        print(0,end=' ')
+    else:
+        print(idx[-1],end=' ')
+
+    stack.append(lst[i])
+    idx.append(i+1)
+
+         
