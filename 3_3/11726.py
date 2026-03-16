2e5a558bf91fe4a3f56c85b5b850213e38ae75c0
kjh <jjjh212212@gmail.com>
2026-03-16 17:52:10 +0900

    11726_2026_03_16

---
+++ 3_3/11726.py
@@ -0,0 +1,12 @@
+from math import factorial as fac
+def com(n,c):
+    return fac(n)//(fac(c)*fac(n-c))
+N=int(input())
+n=N
+c=0
+count=0
+while n >= c:
+    count+=com(n,c)
+    n-=1
+    c+=1
+print(count)
