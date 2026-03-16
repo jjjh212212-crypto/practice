90dfea843fb19a672deb6d78f2ffa779c13d2aa1
kjh <jjjh212212@gmail.com>
2026-03-16 17:43:52 +0900

    11659_PrefixSum_2026_03_16

--- 3_3/11659.py
+++ 3_3/11659.py
@@ -3,10 +3,12 @@ input=sys.stdin.readline
 
 N,M=map(int,input().strip().split())
 lst=list(map(int,input().strip().split()))
-lst2=[0]*N
+lst2 = [0]*N
 lst2[0] = lst[0]
+
 for i in range(1,len(lst)): 
     lst2[i] = lst[i] + lst2[i-1]
+
 for _ in range(M):
     i,j=map(int,input().strip().split())
     if i == 1:
