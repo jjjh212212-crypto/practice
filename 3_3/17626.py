bee0e848374059209fe2bcb494f32646a52f7e1f
kjh <jjjh212212@gmail.com>
2026-03-17 17:41:05 +0900

    17626_hard_DPver_2026_03_17

--- 3_3/17626.py
+++ 3_3/17626.py
@@ -1,3 +1,4 @@
+# 내 풀이
 from math import sqrt
 def mx_pro():
     for i in range(int_n,int(sqrt(n/cnt))-1,-1):
@@ -32,4 +33,18 @@ for i in range(1,5):
         break
 print(result)
 
-        
+# 제미나이 ver (DP)
+n = int(input())
+
+# 1. DP 테이블 초기화
+# dp[i]는 최악의 경우 1의 제곱으로만 이루어져 있으므로 i개입니다.
+# (예: 5 = 1^2 + 1^2 + 1^2 + 1^2 + 1^2 -> 5개)
+dp = [i for i in range(n + 1)]
+
+# 2. 1부터 n까지 작은 숫자부터 차례대로 정답을 채워나감 (Bottom-Up)
+for i in range(1, n + 1):
+    # i보다 작거나 같은 모든 제곱수(j*j)를 빼보면서 최솟값 갱신
+    for j in range(1, int(i**0.5) + 1):
+        dp[i] = min(dp[i], dp[i - j*j] + 1)
+
+print(dp[n])
